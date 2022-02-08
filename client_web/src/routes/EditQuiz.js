b53b7e0ef7268abb3f79838e452dfcb8632a6d88
woogy4231 <woogy4231@naver.com>
2022-02-08 10:57:17 +0900

    update UI

--- client_web/src/routes/EditQuiz.js
+++ client_web/src/routes/EditQuiz.js
@@ -14,11 +14,12 @@ const EditQuiz = () => {
     const date = now.getFullYear() + '.' + (now.getMonth()+1) + '.' + now.getDate();
 
     const [quiz, setQuiz] = useState(location.state.quiz);
-    const quizID = useState(quiz._id);
+    const quizID = useState(location.state.quiz._id);
     const [title, setTitle] = useState(quiz.title);
     const [problems, setProblems] = useState(quiz.problems);
     const [inputs, setInputs] = useState({ newQuestion: '', newAnswer: '' });
     const { newQuestion, newAnswer } = inputs;
+    const [textvisible, setTextVisible] = useState(false);
 
     // 퀴즈 제목 수정
     const onChangeTitle = (e) => { setTitle(e.target.value); }
@@ -74,6 +75,13 @@ const EditQuiz = () => {
 
     // 문제 추가 버튼 클릭
     const onCreate = () => {
+        // 입력값 확인 => 경고문구 show/hidden
+        if(newQuestion=="" || newAnswer==""){
+            setTextVisible(true);
+            return;
+        } else {
+            setTextVisible(false);
+        }
         var num = problems.length+1;
         setProblems((prev) => [
             ...prev,
@@ -88,37 +96,76 @@ const EditQuiz = () => {
 
     // 퀴즈 수정 완료 버튼 클릭
     const onSubmit = () =>{
-        problems.map((value, index) => {
-            value.round = index+1;
-        });
+        // 퀴즈이름 확인 => 경고창
+        if(title==""){
+            alert("퀴즈 이름을 입력하세요.");
+            return;
+        } 
 
-        var newQuiz = {
-            manager: quiz.manager,
-            problem_num: problems.length,
-            problems: problems,
-            title: title,
-            date: date
+        var checkInput = true;
+        // 문제/정답 입력값 확인 => 경고창 or DB에 저장
+        for(var i=0; i<problems.length; i++){
+            if(problems[i].question=="" || problems[i].answer==""){
+                alert("문제와 정답을 모두 입력하세요.");
+                checkInput = false;
+                break;
+            }
         }
-        console.log('newQuiz:', newQuiz);
 
-        // 기존 quiz 삭제
-        socket.emit("drop_ID", quizID[0]); // ### socket으로 id 송신 ###
+        if(checkInput==true){
+            problems.map((value, index) => {
+                value.round = index+1;
+            });
+    
+            var newQuiz = {
+                manager: quiz.manager,
+                problem_num: problems.length,
+                problems: problems,
+                title: title,
+                date: date
+            }
+            console.log('newQuiz:', newQuiz);
+    
+            // 기존 quiz 삭제
+            socket.emit("drop_ID", quizID[0]); // ### socket으로 id 송신 ###
+    
+            // 새로운 quiz 추가
+            socket.emit("quiz", newQuiz); // ### socket으로 서버에 폼에 입력한 데이터 송신 ###
+    
+            // 퀴즈 목록 페이지로 이동
+            navigate("/dynamic-web_OXGame/managequiz", {state : {nickname : nickname}});           
+        }       
+    }
 
-        // 새로운 quiz 추가
-        socket.emit("quiz", newQuiz); // ### socket으로 서버에 폼에 입력한 데이터 송신 ###
-
-        // 퀴즈 목록 페이지로 이동
-        navigate("/dynamic-web_OXGame/managequiz", {state : {nickname : nickname}});
+    // HOME 버튼 클릭 => HOME으로 이동
+    const onClickHome = () => {
+        navigate('/dynamic-web_OXGame',{state : {nickname : nickname}});
     }
 
     return (
         <div>
-            <h1>EDIT QUIZ PAGE</h1>
-            <h3>TITLE: <input type="text" value={title} onChange={onChangeTitle}/></h3>
-            {/*===============================================================*/}
-            <table style={{margin: 'auto'}}>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+                <div class="container-fluid">
+                <a class="navbar-brand" onClick={onClickHome}>HOME</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+            </nav>
+
+            <br/><h1>EDIT QUIZ PAGE</h1><br/>
+            <div>
+                <table style={{margin: 'auto', width: '50%'}}>
+                    <th style={{paddingTop: '1rem'}}>퀴즈 이름</th>
+                    <td><input type="text" class="form-control" value={title} onChange={onChangeTitle}/></td>
+                </table>
+            </div>
+            <br></br>
+            
+
+            <table class="table" style={{margin: 'auto', width: '80%'}}>
                 <thead>
-                    <tr>
+                    <tr class="table-primary">
                         <td>idx</td>
                         <td>Question</td>
                         <td>Answer</td>
@@ -126,31 +173,33 @@ const EditQuiz = () => {
                     </tr>
                 </thead>
 
-                {problems.map((value, index) => (
+                <tbody>
+                    {problems.map((value, index) => (
+                        <tr>
+                            <td><label style={{padding: '0.5rem'}}>Q{index+1}</label></td>
+                            <td><input name="question" class="form-control" style={{padding: '0.5rem'}} id={"question-"+String(index+1)} value={value.question} onChange={onChangeQuestion}/></td>
+                            <td class="form-group">
+                                <input type="radio" class="form-check-input" style={{margin: '0.5rem'}} id={"answer-"+String(index+1)} checked={value.answer==="true"} value="true"  name={"answer-"+String(index+1)} onClick={onChangeOption} /> <label style={{padding: '0.5rem'}}>O &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</label>
+                                <input type="radio" class="form-check-input" style={{margin: '0.5rem'}} id={"answer-"+String(index+1)} checked={value.answer==="false"} value="false" name={"answer-"+String(index+1)} onClick={onChangeOption} /> <label style={{padding: '0.5rem'}}>X</label>
+                            </td>
+                            <td><button onClick={onClickDelete} id={"problem-"+String(index+1)} class="btn btn-secondary btn-sm" style={{margin: '0.5rem'}}>문제 삭제</button></td>
+                        </tr>   
+                    ))}
                     <tr>
-                        <td>Q{index+1}.</td>
-                        <td><input name="question" id={"question-"+String(index+1)} value={value.question} onChange={onChangeQuestion}/></td>
-                        <td>
-                            <input type="radio" id={"answer-"+String(index+1)} checked={value.answer==="true"} value="true"  name={"answer-"+String(index+1)} onClick={onChangeOption} /> true
-                            <input type="radio" id={"answer-"+String(index+1)} checked={value.answer==="false"} value="false" name={"answer-"+String(index+1)} onClick={onChangeOption} /> false
+                        <td></td>
+                        <td><input class="form-control" style={{padding: '0.5rem'}} name="newQuestion" value={newQuestion} onChange={onChange}  /></td>
+                        <td class="form-group">
+                            <input type="radio" class="form-check-input" style={{margin: '0.5rem'}} value="true"  name="newAnswer" checked={newAnswer==='true'} onChange={onChange} /> <label style={{padding: '0.5rem'}}>O &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</label>
+                            <input type="radio" class="form-check-input" style={{margin: '0.5rem'}} value="false" name="newAnswer" checked={newAnswer==='false'} onChange={onChange} /> <label style={{padding: '0.5rem'}}>X</label>
                         </td>
-                        <td><button onClick={onClickDelete} id={"problem-"+String(index+1)}>문제 삭제</button></td>
+                        <td><button class="btn btn-outline-primary btn-sm" style={{margin: '0.5rem'}} onClick={onCreate}>문제 추가</button></td>
                     </tr>
-                        
-                ))}
+                </tbody>
             </table>
-            {/*===============================================================*/}
-            <h5>
-                질문 <input name="newQuestion" value={newQuestion} onChange={onChange}  />
-                <input type="radio" value="true"  name="newAnswer" checked={newAnswer==='true'} onChange={onChange} /> true
-                <input type="radio" value="false" name="newAnswer" checked={newAnswer==='false'} onChange={onChange} /> false
-                <button onClick={onCreate}>문제 추가</button>
-            </h5>
+            { textvisible ? <p class="text-danger">문제와 정답을 모두 입력하세요.</p> : null }
 
-            {/*===============================================================*/}
             <br/>
-            <br/>
-            <button onClick={onSubmit}>퀴즈 수정 완료</button>            
+            <button class="btn btn-primary" onClick={onSubmit}>퀴즈 수정 완료</button>            
             
         </div>
 
--- client_web/src/routes/ManageQuiz.js
+++ client_web/src/routes/ManageQuiz.js
@@ -1,8 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import {socket} from '../components';
-import {useNavigate, useLocation } from 'react-router-dom';
+import {useNavigate, useLocation} from 'react-router-dom';
 
 const ManageQuiz = () => {
+
+    //let history = useHistory();
     
     const navigate = useNavigate();
 
@@ -22,7 +24,7 @@ const ManageQuiz = () => {
                 quizData.push(data[i]); 
             }          
         })        
-    },[]);
+    },[location.key]);
 
     // 퀴즈 리스트 클릭 => 해당 퀴즈 EDIT PAGE로 이동
     const onClickTitle = (e) => {         
@@ -40,6 +42,10 @@ const ManageQuiz = () => {
     // 퀴즈 출제 버튼 클릭 => 출제 페이지로 이동
     const onClickCreate = (e) => {
         navigate("/dynamic-web_OXGame/createquiz",{state : {nickname : nickname}});
+        // history.push({
+        //     pathname: "/dynamic-web_OXGame/createquiz",
+        //     state : {nickname : nickname}
+        // })
     }
 
     // HOME 버튼 클릭 => HOME으로 이동
@@ -50,32 +56,47 @@ const ManageQuiz = () => {
 
     return (
         <div>
-            <h1>MY QUIZ LIST</h1>
-            <table border="1" style={{margin: 'auto'}}>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" onClick={onClickHome}>HOME</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+            </nav>
+
+            <br/><h1>MY QUIZ LIST</h1><br/>
+            <table class="table table-hover" style={{margin: 'auto', width: '80%'}}>
                 <thead>
-                    <tr>
-                        <td>idx</td>
-                        <td>title</td>
-                        <td>problem_num</td>
-                        <td>date</td>
-                        <td>function</td>
+                    <tr class="table-primary bg-primary">
+                        <td><label style={{padding: '0.5rem'}}>idx</label></td>
+                        <td><label style={{padding: '0.5rem'}}>title</label></td>
+                        <td><label style={{padding: '0.5rem'}}>problem_num</label></td>
+                        <td><label style={{padding: '0.5rem'}}>date</label></td>
+                        <td><label style={{padding: '0.5rem'}}>function</label></td>
                     </tr>
                 </thead>
-                {quizData.map((value, index) => (
-                    <tr id={index} >
-                        <td id={index} onClick={onClickTitle}>{index+1}</td>
-                        <td id={index} onClick={onClickTitle}>{value.title}</td>
-                        <td id={index} onClick={onClickTitle}>{value.problem_num}</td>
-                        <td id={index} onClick={onClickTitle}>{value.date}</td>
-                        <td><button onClick={onClickDelete} id={value._id}>퀴즈 삭제</button></td>
-                    </tr>
-                        
-                ))}
+
+                <tbody>
+                    {quizData.map((value, index) => (
+                        <tr id={index} >
+                            <td id={index} onClick={onClickTitle}><label style={{padding: '0.5rem'}}>{index+1}</label></td>
+                            <td id={index} onClick={onClickTitle}><label style={{padding: '0.5rem'}}>{value.title}</label></td>
+                            <td id={index} onClick={onClickTitle}><label style={{padding: '0.5rem'}}>{value.problem_num}</label></td>
+                            <td id={index} onClick={onClickTitle}><label style={{padding: '0.5rem'}}>{value.date}</label></td>
+                            <td><button class="btn btn-secondary btn-sm" style={{margin: '0.5rem'}} onClick={onClickDelete} id={value._id}>퀴즈 삭제</button></td>
+                        </tr>
+                            
+                    ))}
+                </tbody>
+                
             </table>
+            <br />
+            {quizData.length<1 ? <p class="text-primary">생성된 퀴즈가 없습니다.</p> : null}
             <br/>
-            <button onClick={onClickCreate}>퀴즈 출제하러 가기</button> 
+            <button class="btn btn-outline-primary" onClick={onClickCreate}>새 퀴즈 생성</button> 
             <br/>
-            <button onClick={onClickHome}>HOME</button>    
+            {/* <button class="btn btn-primary" onClick={onClickHome}>HOME</button>     */}
         </div>
 
     );
